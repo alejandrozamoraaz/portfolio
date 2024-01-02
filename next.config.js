959360b592dcfa90d8d32c137e9e312ac6f81aaa
alejandrozamoraaz <88607814+alejandrozamoraaz@users.noreply.github.com>
2024-01-01 21:54:40 -0600

    test

--- next.config.js
+++ next.config.js
@@ -3,7 +3,16 @@ const nextConfig = {
     output: 'export', //descomentariar al hacer commit
     images: {
         loader: 'custom',
-        loaderFile: './cloudinary-loader.ts'
+        loaderFile: './cloudinary-loader.ts',
+        domains: ['res.cloudinary.com'],
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'res.cloudinary.com',
+                pathname: '/dlyfwab7x/image/upload/**',
+            },
+        ],
+
         // unoptimized: true
     },
 };
