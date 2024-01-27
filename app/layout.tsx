5fd4feffd0335b222bd26a0433d6757342302ad3
alejandrozamoraaz <88607814+alejandrozamoraaz@users.noreply.github.com>
2024-01-26 18:44:06 -0600

    Added: Google analytics tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -103,6 +103,9 @@ export default function RootLayout({ children }: { children: React.ReactNode; })
               <Text classText="flex-end p-y-small" type="bodySmall" text={t('developed_in', { ns: 'common' })} />
               <Text classText="flex-end" weight={300} type="bodySmall" text={t('based_in', { ns: 'common' })} />
               <Text classText="flex-end" weight={300} type="bodySmall" text="Version 1.0" />
+              <div className="flex-end p-y-small">
+                <Chip text={t('tag_google_analytics', { ns: 'common' })} />
+              </div>
             </div>
           </footer>
         </main>
